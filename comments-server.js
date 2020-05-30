576fd010f6474e393aef7fffdfc37bb7eef10ee4
Griffin Banta <gbanta@uoregon.edu>
2020-05-30 09:28:08 -0700

    finished server 9 and comments server

--- comments-server.js
+++ comments-server.js
@@ -11,12 +11,12 @@ const port = 8888,
   dbURL = "mongodb://127.0.0.1:27017",
   MongoClient = require("mongodb").MongoClient,
   client = new MongoClient(dbURL),
-  dbName = "phonebookDB",
-  collName = "phonebook";
+  dbName = "commentsDB",
+  collName = "comments";
 
 let db,
   col,
-  db_length = 0;
+  key = 0;
 
 MongoClient.connect(
   dbURL,
@@ -51,8 +51,8 @@ app.use((req, res, next) => {
   next();
 });
 
-// endpoint to Get all tel numbers
-app.get("/telnums", (req, res) => {
+// endpoint to Get all comments
+app.get("/comments", (req, res) => {
   db.collection(collName)
     .find({})
     .toArray(function (err, result) {
@@ -60,62 +60,58 @@ app.get("/telnums", (req, res) => {
       //console.log(result);
       res.status(200).send({
         success: "true",
-        message: "tel numbers retrieved successfully",
-        tel_nums: result,
+        message: "comments retrieved successfully",
+        comments: result,
       });
-      //mongoDBclient.close();
     });
 });
 
-//endpoint to get a single tel number
-//Examples: /telnum/1, /telnum/7, /telnum/11, ...
-app.get("/telnum/:id", (req, res, next) => {
-  const id = parseInt(req.params.id);
+//endpoint to get a single comment
+app.get("/getcomment/:data", (req, res, next) => {
+  const data = req.params.data;
 
   db.collection(collName).findOne(
     {
-      _id: id,
+      data: data,
     },
     function (err, result) {
       if (err) throw err;
       console.log(result);
       res.status(200).send({
         status_code: 200,
-        message: result ? result : "document not found",
+        message: result ? result : "comment not found",
       });
     }
   );
 });
 
-//Endpoint to add a tel number
-app.post("/addnum", (req, res) => {
+//Endpoint to add a comment
+app.post("/addcomment", (req, res) => {
   // Insert a single document
-  let telnum = {
-    _id: (db_length += 1),
-    name: req.body.name,
-    number: req.body.number,
+  let comment = {
+    data: req.body.data,
   };
 
   db.collection(collName)
-    .insertOne(telnum)
+    .insertOne(comment)
     .then((result) => {
-      //console.log(`record inserted ${result}`)
+      console.log(`record inserted ${result}`);
       return res.status(201).send({
         status_code: 200,
-        message: "Telephone Number added successfully",
-        telnum,
+        message: "Comment added successfully",
+        comment,
       });
     })
     .catch((error) => console.error(error));
 });
 
-//Endpoint to Delete a single tel number
-//Examples: /deletenum/1, /deletenum/7, /deletenum/11, ...
-app.delete("/deletenum/:id", (req, res) => {
-  const id = parseInt(req.params.id);
+//Endpoint to Delete a single comment
+app.post("/deletecomment/:data", (req, res) => {
+  const data = req.params.data;
+  console.log(data);
   db.collection(collName).deleteOne(
     {
-      _id: id,
+      data: data,
     },
     function (err, obj) {
       if (err) throw err;
@@ -126,7 +122,7 @@ app.delete("/deletenum/:id", (req, res) => {
 
   return res.status(200).send({
     status_code: 200,
-    message: "Telephone number deleted",
+    message: "comment deleted",
   });
 });
 
